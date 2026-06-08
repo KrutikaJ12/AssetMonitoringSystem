f8c90dcd37e3497ecc19fcaa10cd312b6859aa66
krutikaj <krutikats78@gmail.com>
2026-06-08 12:54:29 +0530

    feat:creation of statistics card for reusuability and removed unused code

--- FrontEnd/src/pages/Dashboard/Home.tsx
+++ FrontEnd/src/pages/Dashboard/Home.tsx
@@ -1,10 +1,11 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+import EcommerceMetrics from "../../components/ecommerce/StatisticsMetrics";
 import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
+import StatisticsMetrics from "../../components/ecommerce/StatisticsMetrics";
 
 export default function Home() {
   return (
@@ -13,24 +14,26 @@ export default function Home() {
         title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <div className="grid grid-cols-12 gap-4 md:gap-6 border border-red-500">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
+      {/* <div className="grid grid-cols-12 gap-4 md:gap-6"> */}
+      {/* <div className="col-span-12 space-y-6 xl:col-span-7 border"> */}
+      <div>
+        <div className="border grid gap-2 w-full">
+          <StatisticsMetrics />
 
-          <MonthlySalesChart />
+          {/* <MonthlySalesChart /> */}
         </div>
 
-        <div className="col-span-12 xl:col-span-5">
+        {/* <div className="col-span-12 xl:col-span-5">
           <MonthlyTarget />
-        </div>
+        </div> */}
 
-        <div className="col-span-12">
+        {/* <div className="col-span-12">
           <StatisticsChart />
-        </div>
+        </div> */}
 
-        <div className="col-span-12 xl:col-span-5">
+        {/* <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
-        </div>
+        </div> */}
 
         <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
