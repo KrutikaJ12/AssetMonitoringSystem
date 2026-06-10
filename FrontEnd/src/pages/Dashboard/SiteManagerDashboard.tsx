79ceab739053fe8cb21bcecd1b0cbdd0e5f0a0f9
krutikaj <krutikats78@gmail.com>
2026-06-10 11:10:03 +0530

    feat(site-manager): add site manager dashboard page

--- FrontEnd/src/pages/Dashboard/SiteManagerDashboard.tsx
+++ FrontEnd/src/pages/Dashboard/SiteManagerDashboard.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
 
+import StatisticsMetrics from '../../components/dashboard/StatisticsMetrics'
+import { GroupIcon } from '../../icons';
+const siteManagerMetrics = [
+  {
+    title: "Site Assets",
+    value: 25,
+    icon: <GroupIcon />,
+  },
+  {
+    title: "Active Assets",
+    value: 18,
+    icon: <GroupIcon />,
+  },
+  {
+    title: "Idle Assets",
+    value: 4,
+    icon: <GroupIcon />,
+  },
+  {
+    title: "Maintenance Due",
+    value: 3,
+    icon: <GroupIcon />,
+  },
+  {
+    title: "Operators",
+    value: 12,
+    icon: <GroupIcon />,
+  },
+];
 const SiteManagerDashboard = () => {
   return (
-    <div>SiteManagerDashboard</div>
+    <div>
+      <StatisticsMetrics metrics={siteManagerMetrics}/>
+    </div>
   )
 }
 
