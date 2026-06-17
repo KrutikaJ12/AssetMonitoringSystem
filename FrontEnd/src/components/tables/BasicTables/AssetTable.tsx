b3060dc76820048ad329ceb51a03c1f3511feaac
krutikaj <krutikats78@gmail.com>
2026-06-17 11:32:41 +0530

    refactor:added filterting functionality for assets in admin panel

--- FrontEnd/src/components/tables/BasicTables/AssetTable.tsx
+++ FrontEnd/src/components/tables/BasicTables/AssetTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Badge from "../../ui/badge/Badge";
 import {
   Table,
@@ -6,7 +7,8 @@ import {
   TableHeader,
   TableRow,
 } from "../../ui/table";
-import { Download,Eye } from "lucide-react";
+import { Download, Eye, Search } from "lucide-react";
+import Button from "../../ui/button/Button";
 const assetsData = [
   {
     assetId: "EX001",
@@ -79,48 +81,69 @@ interface AssetTableProps {
   data: Asset[];
 }
 export function AssetTable(Asset: AssetTableProps) {
+  const [search, setSearch] = useState("");
+  const [selectedSite, setSelectedSite] = useState("");
+  const [status, setStatus] = useState("");
+  const hasFilters = search || selectedSite|| status;
+  //   const handleExport = () => {
+  //     const headers = [
+  //       "Name",
+  //       "Mobile",
+  //       "City",
+  //       "KYC",
+  //       "PaymentStatus",
+  //       "Joined",
+  //     ];
+  //     const csvData = filteredUsers
+  //       .map((user) =>
+  //         [
+  //           user.name,
+  //           user.mobile,
+  //           user.city,
+  //           user.kyc,
+  //           user.paymentStatus || "N/A",
+  //           user.joined,
+  //         ].join(","),
+  //       )
+  //       .join("\n");
 
-//   const handleExport = () => {
-//     const headers = [
-//       "Name",
-//       "Mobile",
-//       "City",
-//       "KYC",
-//       "PaymentStatus",
-//       "Joined",
-//     ];
-//     const csvData = filteredUsers
-//       .map((user) =>
-//         [
-//           user.name,
-//           user.mobile,
-//           user.city,
-//           user.kyc,
-//           user.paymentStatus || "N/A",
-//           user.joined,
-//         ].join(","),
-//       )
-//       .join("\n");
+  //     const blob = new Blob([[headers.join(","), csvData].join("\n")], {
+  //       type: "text/csv",
+  //     });
+  //     const url = URL.createObjectURL(blob);
+  //     const link = document.createElement("a");
+  //     link.href = url;
+  //     link.download = "riders_data.csv";
+  //     link.click();
 
-//     const blob = new Blob([[headers.join(","), csvData].join("\n")], {
-//       type: "text/csv",
-//     });
-//     const url = URL.createObjectURL(blob);
-//     const link = document.createElement("a");
-//     link.href = url;
-//     link.download = "riders_data.csv";
-//     link.click();
+  //     // setToastType("success");
+  //     // setToastMessage("Data exported successfully!");
+  //     // setTimeout(() => setToastMessage(null), 3000);
+  //   };
 
-//     // setToastType("success");
-//     // setToastMessage("Data exported successfully!");
-//     // setTimeout(() => setToastMessage(null), 3000);
-//   };
+  const sites = [
+    { code: "MUM", name: "Mumbai" },
+    { code: "PUN", name: "Pune" },
+    { code: "VAS", name: "Vashi" },
+  ];
+  const filteredData = assetsData.filter((data) => {
+    const matchesSearch =
+      data.assetName.toLowerCase().includes(search.toLowerCase()) ||
+      data.category.toLowerCase().includes(search.toLowerCase());
+      const matchSite = selectedSite ? data.site == selectedSite :true
+    return matchesSearch && matchSite;
+  });
+  console.log(filteredData, "filter");
+  const clearFilters = () => {
+    setSearch("");
+    setSelectedSite("")
+  };
   return (
     <>
       <div className=" h-10 flex justify-between mb-4  ">
         <div></div>
         <button
-        //   onClick={handleExport}
+          //   onClick={handleExport}
           className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:bg-white/[0.03] dark:text-gray-400 transition"
         >
           <Download size={16} />
@@ -128,14 +151,60 @@ export function AssetTable(Asset: AssetTableProps) {
         </button>
       </div>
       <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
-      
+        {/* Search + City/Branch Filters */}
+        <div className="mb-5 flex flex-col gap-3 sm:flex-row sm:items-center sm:flex-wrap">
+          {/* Search */}
+          <div className="relative flex-1 min-w-[200px]">
+            <Search
+              className="absolute left-3 top-2.5 text-gray-400"
+              size={18}
+            />
+            <input
+              type="text"
+              placeholder="Search by assets by name,category..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 bg-gray-50 text-sm focus:border-brand-500 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-white"
+            />
+          </div>
+          <Button size="sm" variant="outline">
+            Status
+          </Button>
+
+          {/* Sites Filter */}
+          <select
+            value={selectedSite}
+            onChange={(e) => {
+              setSelectedSite(e.target.value);
+            }}
+            className="rounded-lg border border-gray-300 bg-gray-50 px-3 py-2 text-sm text-gray-700 focus:border-brand-500 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-white min-w-[150px]"
+          >
+            <option value="">All sites</option>
+            {sites.map((site) => (
+              <option key={site.code} value={site.name}>
+                {site.name}
+              </option>
+            ))}
+          </select>
+
+
+          {/* Clear filters */}
+          {hasFilters && (
+            <button
+              onClick={clearFilters}
+              className="text-sm font-medium text-brand-600 hover:text-brand-700 dark:text-brand-400 whitespace-nowrap"
+            >
+              Clear filters
+            </button>
+          )}
+        </div>
+
         <div className="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
-          <div>
+          {/* <div>
             <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
               Asset Allocation
             </h3>
-          </div>
-
+          </div> */}
         </div>
         <div className="max-w-full overflow-x-auto">
           <Table>
@@ -152,13 +221,13 @@ export function AssetTable(Asset: AssetTableProps) {
                   isHeader
                   className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                 >
-                 Name
+                  Name
                 </TableCell>
                 <TableCell
                   isHeader
                   className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                 >
-                 Category
+                  Category
                 </TableCell>
                 <TableCell
                   isHeader
@@ -226,7 +295,7 @@ export function AssetTable(Asset: AssetTableProps) {
             {/* Table Body */}
 
             <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
-              {assetsData.map((site) => (
+              {filteredData.map((site) => (
                 <TableRow className="">
                   <TableCell className="py-3">
                     <div className="flex items-center gap-3">
@@ -284,7 +353,7 @@ export function AssetTable(Asset: AssetTableProps) {
                     {site.fuelLevel}
                   </TableCell>
                   <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                    <Eye/>
+                    <Eye />
                   </TableCell>
                   {/* <TableCell>
                     <div className="flex items-center gap-2">
