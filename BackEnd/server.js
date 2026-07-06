37d1b934f14d82bd7ba081a82a361534a2709e50
krutikaj <krutikats78@gmail.com>
2026-07-06 16:26:02 +0530

    feat: implement dashboard overview API with summary and chart data

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const tokenRoutes = require("./src/routes/tokenRoutes");
 const vehicleRoutes = require("./src/routes/vehicleRoutes");
+const dashboardRoutes = require("./src/routes/dashboardRoutes");
 const { connectDB } = require("./src/config/db")
 connectDB();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use("/api", tokenRoutes);
 app.use("/api", vehicleRoutes);
-
+app.use("/api", dashboardRoutes);
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
