8020214ee08d118634dcaa55e05b2d808bb47dbf
krutikaj <krutikats78@gmail.com>
2026-07-24 15:46:22 +0530

    "feat:installation of dependecies and addition of authroutes"

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,23 +2,34 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 const tokenRoutes = require("./src/routes/tokenRoutes");
 const vehicleRoutes = require("./src/routes/vehicleRoutes");
 const dashboardRoutes = require("./src/routes/dashboardRoutes");
+const authRoutes = require("./src/routes/authRoutes");
 const { connectDB } = require("./src/config/db")
-connectDB();
+// connectDB();
 
 const app = express();
-
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", tokenRoutes);
 app.use("/api", vehicleRoutes);
 app.use("/api", dashboardRoutes);
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+    });
+};
+
+startServer();
