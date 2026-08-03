5b67a44b438a2180e23ab935f46be8c23b388cf6
krutikaj <krutikats78@gmail.com>
2026-08-03 15:34:56 +0530

    "feat:showing of permissions bases ui"

--- FrontEnd/src/components/tables/BasicTables/AssetTable.tsx
+++ FrontEnd/src/components/tables/BasicTables/AssetTable.tsx
@@ -12,6 +12,7 @@ import Button from "../../ui/button/Button";
 // import Drawer from "../../ui/Drawer/Drawer";
 import { Modal } from "../../ui/modal";
 import AssetDetails from "./AssetDetails";
+import { useAuth } from "../../../hooks/useAuth";
 const assetsData = [
   {
     assetId: "EX001",
@@ -128,7 +129,7 @@ export function AssetTable(Asset: AssetTableProps) {
     setSearch("");
     setSelectedSite("");
   };
-
+   const {hasPermission}=useAuth()
   const handleExport = () => {
     const headers = [
       "Asset ID",
@@ -175,13 +176,16 @@ export function AssetTable(Asset: AssetTableProps) {
     <>
       <div className=" h-10 flex justify-between mb-4  ">
         <div></div>
-        <button
+        {hasPermission("ASSET_EXPORT") && (
+          <button
           onClick={handleExport}
           className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:bg-white/[0.03] dark:text-gray-400 transition"
         >
           <Download size={16} />
           Export
         </button>
+        )}
+        
       </div>
       <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
         {/* Search + City/Branch Filters */}
@@ -297,12 +301,15 @@ export function AssetTable(Asset: AssetTableProps) {
                 >
                   Fuel %
                 </TableCell>
-                <TableCell
+                {hasPermission("ASSET_VIEW") && (
+                  <TableCell
                   isHeader
                   className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                 >
                   View
                 </TableCell>
+                )}
+                
                 {/* <TableCell
                   isHeader
                   className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
@@ -384,6 +391,8 @@ export function AssetTable(Asset: AssetTableProps) {
                   <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
                     {site.fuelLevel}
                   </TableCell>
+                  {
+                   hasPermission("ASSET_VIEW") && (
                   <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
                     <button
                       onClick={() => {
@@ -394,6 +403,7 @@ export function AssetTable(Asset: AssetTableProps) {
                       <Eye />
                     </button>
                   </TableCell>
+                    )}
                   {/* <TableCell>
                     <div className="flex items-center gap-2">
                       <div className="h-2 w-20 rounded-full bg-gray-200">
