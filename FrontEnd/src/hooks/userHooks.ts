35caf1dd8179710b75c11985f0bcac90b10a4df7
Saquib Ansari <saquib9698@gmail.com>
2026-08-18 16:02:32 +0530

    hook fike add

---
+++ FrontEnd/src/hooks/userHooks.ts
@@ -0,0 +1,61 @@
+import {
+  createUserData,
+  getUsersData,
+  updateUserData,
+  deleteUserData,
+} from "../api/userApi";
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+// GET - Read Users
+export const useUsers = () => {
+  return useQuery({
+    queryKey: ["/users"],
+    queryFn: getUsersData,
+  });
+};
+
+// POST - Create User
+export const useCreateUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createUserData,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["/users"],
+      });
+    },
+  });
+};
+
+// PUT - Update User
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ userId, userData }) =>
+      updateUserData(userId, userData),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["/users"],
+      });
+    },
+  });
+};
+
+// DELETE - Delete User
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteUserData,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["/users"],
+      });
+    },
+  });
+};
